7c78ba568745f27fe1feb1eb067f51fbbd9c303f
anna.torgova <anna.torgova@lenvendo.ru>
2023-10-19 22:11:19 +0300

    HW9, allure

--- tests/test_steps.py
+++ tests/test_steps.py
@@ -3,16 +3,23 @@ from selene.support import by
 from selene.support.conditions import be
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
+from allure_commons.types import Severity
 
 
 def test_dynamic_steps():
+    allure.dynamic.tag("web")
+    allure.dynamic.severity(Severity.BLOCKER)
+    allure.dynamic.feature("Задачи в репозитории")
+    allure.dynamic.story("Проверка, что в репозитории есть Issue с номером 76")
+    allure.dynamic.link("https://github.com", name="Testing")
+
     with allure.step("Открываем главную страницу"):
         browser.open("https://github.com")
 
-    with allure.step("Ищем репозитория"):
-        s(".header-search-input").click()
-        s(".header-search-input").send_keys("eroshenkoam/allure-example")
-        s(".header-search-input").submit()
+    with allure.step("Ищем репозиторий"):
+        s(".header-search-button").click()
+        s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+        s("#query-builder-test").press_enter()
 
     with allure.step("Переходим по ссылке репозитория"):
         s(by.link_text("eroshenkoam/allure-example")).click()
@@ -24,6 +31,12 @@ def test_dynamic_steps():
         s(by.partial_text("#76")).should(be.visible)
 
 
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "eroshenkoam")
+@allure.feature("Задачи в репозитории")
+@allure.story("Проверка, что в репозитории есть Issue с номером 76")
+@allure.link("https://github.com", name="Testing")
 def test_decorator_steps():
     open_main_page()
     search_for_repository("eroshenkoam/allure-example")
@@ -39,14 +52,14 @@ def open_main_page():
 
 @allure.step("Ищем репозитория {repo}")
 def search_for_repository(repo):
-    s(".header-search-input").click()
-    s(".header-search-input").send_keys(repo)
-    s(".header-search-input").submit()
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    s("#query-builder-test").press_enter()
 
 
 @allure.step("Переходим по ссылке репозитория {repo}")
 def go_to_repository(repo):
-    s(by.link_text(repo)).click()
+    s(by.link_text("eroshenkoam/allure-example")).click()
 
 
 @allure.step("Открываем таб Issues")
